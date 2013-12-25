b178a1651030699b3861a55a3b8cba11a3ca412d
joinedapp <arash_hassibi@yahoo.com>
2013-12-25 20:53:13 +0000

    UPDATE: github to joinedapp dotfiles

--- setup.sh
+++ setup.sh
@@ -26,6 +26,9 @@ npm install cheerio
 # Install restler (REST API)
 npm install restler
 
+# Install express (web application framework)
+npm install express
+
 # Install rlwrap to provide libreadline features with node
 # See: http://nodejs.org/api/repl.html#repl_repl
 sudo apt-get install -y rlwrap
@@ -48,7 +51,7 @@ fi
 if [ -d .emacs.d/ ]; then
     mv .emacs.d .emacs.d~
 fi
-git clone https://github.com/startup-class/dotfiles.git
+git clone https://github.com/joinedapp/dotfiles.git
 ln -sb dotfiles/.screenrc .
 ln -sb dotfiles/.bash_profile .
 ln -sb dotfiles/.bashrc .
